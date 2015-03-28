e79414cb34b75d70ca7d8457209cac4ee375de5d
Jesper Blad Jensen <deldy@deldysoft.dk>
2015-03-28 16:46:20 +0100

    added support for the organizers filter and the cities filter

--- routes/meetings.js
+++ routes/meetings.js
@@ -22,6 +22,14 @@ router.get('/', parseMeetingFilters, function(req, res, next) {
     			query.tags = query.tags || {};
     			query.tags['$in'] = req.filters.tags;
     		}
+    		if(req.filters.organizers) {
+    			query.organizers = query.organizers || {};
+    			query.organizers['$in'] = req.filters.organizers;
+    		}
+    		if(req.filters.cities) {
+    			query.city = query.city || {};
+    			query.city['$in'] = req.filters.cities;
+    		}
 
             var meetings = db.collection('meetings');
             meetings.find(query).toArray(function (err, items) {
@@ -36,7 +44,9 @@ function parseMeetingFilters(req, res, next) {
 	req.filters = {
 		after: req.query.after,
 		before: req.query.before,
-		tags: req.query.tags ? req.query.tags.split(',') : null
+		tags: req.query.tags ? req.query.tags.split(',') : null,
+		organizers: req.query.organizers ? req.query.organizers.split(',') : null,
+		cities: req.query.cities ? req.query.cities.split(',') : null
 	};
 
 	next();
