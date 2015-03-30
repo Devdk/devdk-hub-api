40c6c6e8554385ef6a176f8328d405ca8fd63306
Jesper Blad Jensen <deldy@deldysoft.dk>
2015-03-30 08:24:02 +0200

    refactoring

--- routes/meetings.js
+++ routes/meetings.js
@@ -1,13 +1,13 @@
 var express = require('express');
+var meeting_query = require('../libs/meetings_query.js');
 var router = express.Router();
-
 var MongoClient = require('mongodb').MongoClient;
 
 var config = require('../config.js');
 
 router.get('/', function(req, res, next) {
-    var filter = buildMeetingFilterFromQuery(req.query);
-    var query = buildMeetingQuery(filter);
+    var filter = meeting_query.parseQueryString(req.query);
+    var query = meeting_query.buildMongoQuery(filter);
 
     MongoClient.connect(config.mongodbUrl, function(err, db) {
         if(!err) {
