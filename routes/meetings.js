badadac073f6aa00db483a25a7e7bc0ebea95f29
Jesper Blad Jensen <deldy@deldysoft.dk>
2015-03-31 16:20:50 +0200

    Added possibility to post a new meeting

--- routes/meetings.js
+++ routes/meetings.js
@@ -5,7 +5,7 @@ var MongoClient = require('mongodb').MongoClient;
 
 var config = require('../config.js');
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     var filter = meeting_query.parseQueryString(req.query);
     var query = meeting_query.buildMongoQuery(filter);
 
@@ -18,5 +18,42 @@ router.get('/', function(req, res, next) {
         }
     });
 });
+
+router.post('/', function(req, res) {
+    var meeting = createMeetingFrom(req.body);
+    meeting.created_at = new Date().getTime();
+
+    MongoClient.connect(config.mongodbUrl, function(err, db) {
+        if(!err) {
+            var meetings = db.collection('meetings');
+            meetings.insert(meeting, function(err, doc) {
+                res.send(doc.ops[0]);
+            });
+        }
+    });
+});
+
+function createMeetingFrom(data) {
+    var attributes = ["title", "starts_at", "description", "url", "tags", "organizers", "city"];
+    var parsers = {
+        "starts_at": Date.new,
+        "created_at": Date.new
+    }
+    var newObject = {};
+    for(var x = 0; x<= attributes.length; x++) {
+        var attribute = attributes[x];
+        if(data[attribute] !== undefined) {
+            var parser = parsers[attribute] !== undefined ? parsers[attribute] : nullParser;
+            
+            newObject[attribute] = parser(data[attribute]);
+        }
+    }
+
+    return newObject;
+}
+
+function nullParser(x) {
+    return x;
+}
 
 module.exports = router;
