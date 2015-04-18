b436dae1d3c4f4c9a5634bf598570442e75a04bd
Jesper Blad Jensen <deldy@deldysoft.dk>
2015-04-18 17:55:58 +0200

    fixed filter

--- libs/meetings.js
+++ libs/meetings.js
@@ -42,9 +42,6 @@ var ValidationError = function(validationResult) {
 }
 
 module.exports.find = function(query, cb) {
-	var filter = meeting_query.parseQueryString({});
-    var query = meeting_query.buildMongoQuery(filter);
-
     var meetings = mongodb.db.collection('meetings');
     meetings.find({query: query, $orderby: { starts_at : 1 } } ).toArray(cb);
 
