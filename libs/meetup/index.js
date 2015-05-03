d02244ae4808169c1d879eaa3dd2459630270f4c
Jesper Blad Jensen <deldy@deldysoft.dk>
2015-05-03 14:56:37 +0200

    Added information from groups

--- libs/meetup/index.js
+++ libs/meetup/index.js
@@ -1,20 +1,25 @@
 var api = require("./api");
 
-module.exports.getMeetingsFromGroup = function(groupName, cb) {
-  api.getGroupEvents(groupName, function(err, data) {
+module.exports.getMeetingsFromGroup = function(groupInformation, cb) {
+  api.getGroupEvents(groupInformation.meetupUrl, function(err, data) {
     if(err) {
       cb(err);
     }
     
-    var output = data.results.map(meetupEventToMeeting);
+    var output = data.results.map(function(x) { return meetupEventToMeeting(groupInformation, x); });
     
     cb(null, output);
   });
 };
 
-function meetupEventToMeeting(result) {
+function meetupEventToMeeting(groupInformation, result) {
   return {
     title: result.name,
+    organizers: [
+      groupInformation.organizerName
+    ],
+    city: groupInformation.city,
+    tags: groupInformation.tags,
     description: result.description,
     url: result.event_url,
     meetup_id: result.id,
--- test/libs/meetup/index.js
+++ test/libs/meetup/index.js
@@ -12,14 +12,22 @@ var meetup = proxyquire("../../../libs/meetup/index", {
   }
 });
 
+var groupInformation = {
+  meetupUrl: "dwodense",
+  organizerName: "Design and Web Development Odense",
+  city: "Odense"
+};
+
 describe('Meetup', function(){
-	
+  
 	it('should convert meetup meetings to hub formatted meetings', function(done){
-    meetup.getMeetingsFromGroup("dwodense", function(err, meetings) {
+    meetup.getMeetingsFromGroup(groupInformation, function(err, meetings) {
       assert.equal(meetings.length, 12);
       var meeting = meetings[0];
       
       assert.equal(meeting.title, "Månedligt DWOdense meetup");
+      assert.equal(meeting.organizers[0], groupInformation.organizerName);
+      assert.equal(meeting.city, groupInformation.city);
       assert(meeting.description != null);
       assert.equal(meeting.url, "http://www.meetup.com/dwodense/events/219578700/");
       assert.equal(meeting.starts_at, "Tue May 12 2015 19:00:00 GMT+0200 (CEST)");
