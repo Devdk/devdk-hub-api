9076ce1baaae1654f66ccab699d1795983f4457f
Jesper Blad Jensen <deldy@deldysoft.dk>
2015-05-27 22:04:29 +0200

    test refactoring

--- test/libs/meetings_query.js
+++ test/libs/meetings_query.js
@@ -14,27 +14,17 @@ describe('Meetings', function(){
 	    	};
 
   		var result = meetings_query.parseQueryString(query);
-  	    
-  	    it('it should parse after', function(){
-  	    	assert.equal(Date.parse("2015-03-28"), result.after);
-  	    });
-  
-  	    it('it should parse before', function(){
-  	    	assert.equal(Date.parse("2015-04-01"), result.before);
-  	    });
-  
-  	    it('it should parse tags', function(){
-  	    	assert.deepEqual([".NET", "JavaScript"], result.tags);
-  	    });
-  
-  	    it('it should parse organizers', function(){
-  	    	assert.deepEqual(["ONUG", "@condevdk"], result.organizers);
-  	    });
-  
-  	    it('it should parse cities', function(){
-  	    	assert.deepEqual(["Odense", "København"], result.cities);
-  	    });
-    	});
+  	  
+      var expected = {
+        after: Date.parse("2015-03-28"),
+        before: Date.parse("2015-04-01"),
+        tags: [".NET", "JavaScript"],
+        organizers: ["ONUG", "@condevdk"],
+        cities: ["Odense", "København"]
+      };
+      
+      assert.deepEqual(result, expected);
+  	});
 
   });
   
