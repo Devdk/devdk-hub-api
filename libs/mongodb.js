c45798bb75d035507b5d245c41647c46f9f84b03
Jesper Blad Jensen <deldy@deldysoft.dk>
2015-05-27 17:14:07 +0200

    refactoring

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,12 +1,17 @@
 var MongoClient = require('mongodb').MongoClient;
 var config = require('../config.js');
 
-module.exports.init = function(callback) {
-	MongoClient.connect(config.mongodbUrl, function(err, db) {
-		if(!err) {
-			module.exports.db = db;
-		}
-
-		return callback(err);
-	});
+var MongoDB = {
+  init: function(callback) {
+  	MongoClient.connect(config.mongodbUrl, function(err, db) {
+  		if(!err) {
+  			this.db = db;
+  		}
+  
+  		return callback(err);
+  	}.bind(this));
+  },
+  db: null
 };
+
+module.exports = MongoDB;
