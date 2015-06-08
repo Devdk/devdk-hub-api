Devdk/devdk-hub-api

commit 3348e251a03fa2e614c57122129dbb1c85fd1937

Update meetup_groups.js
Added Ionic Denmark.

libs/meetup_groups.js
@@ -5,6 +5,7 @@ var MeetupGroups = {
     }));    
   },
   _groups: [
+    { meetupUrl: "ionicdenmark", organizerName; "Ionic Denmark", city: "Århus", tags: ["ionic", "hybrid apps", "web", "mobile apps"] },
     { meetupUrl: "dwodense", organizerName: "Design and Web Development Odense", city: "Odense", tags: [ "web" ] },
     { meetupUrl: "Odense-NET-User-Group", organizerName: "Odense .NET User Group", city: "Odense", tags: [ ".net" ] },
     { meetupUrl: "umbracodk", organizerName: "Umbraco DK", city: "Odense", tags: [ ".net", "umbraco" ] },

commit 0c3873b68b2a30f3929bd6a895488f5889e0d7ea

Fix for syntax error introduced in 3348e251

libs/meetup_groups.js
@@ -5,7 +5,7 @@ var MeetupGroups = {
     }));    
   },
   _groups: [
-    { meetupUrl: "ionicdenmark", organizerName; "Ionic Denmark", city: "Århus", tags: ["ionic", "hybrid apps", "web", "mobile apps"] },
+    { meetupUrl: "ionicdenmark", organizerName: "Ionic Denmark", city: "Århus", tags: ["ionic", "hybrid apps", "web", "mobile apps"] },
     { meetupUrl: "dwodense", organizerName: "Design and Web Development Odense", city: "Odense", tags: [ "web" ] },
     { meetupUrl: "Odense-NET-User-Group", organizerName: "Odense .NET User Group", city: "Odense", tags: [ ".net" ] },
     { meetupUrl: "umbracodk", organizerName: "Umbraco DK", city: "Odense", tags: [ ".net", "umbraco" ] },