c5c9a5eaa1eec158f7f78a3d103eaa0ec752104f
Jesper Blad Jensen <deldy@deldysoft.dk>
2015-09-12 20:23:04 +0200

    refactorings

--- libs/meetup/index.js
+++ libs/meetup/index.js
@@ -10,51 +10,7 @@ function meetupEventToMeeting(groupInformation, result) {
   var city = groupInformation.city;
   var venue = result.venue;
   if(venue) {
-    var lat = venue.lat;
-    var lon = venue.lon;
-    
-    var cities = [
-      { name: "Aarhus", lat: 56.178104, lon: 10.1817985 },
-      { name: "Odense", lat: 55.3842478, lon: 10.3978195 },
-      { name: "København", lat: 55.6712674, lon: 12.5608388 },
-      { name: "Esbjerg", lat: 55.5225198, lon: 8.4635049 },
-      { name: "Aalborg", lat: 57.0268098, lon: 9.90782 }
-    ];
-    
-    cities.forEach(function(city) {
-      city.distance = getDistance(lat, lon, city.lat, city.lon);
-    });
-    
-    var closeCities = cities.filter(function(city) { return city.distance <= 50; });
-    
-    if(closeCities.length > 0) {
-      var closest = _.min(closeCities, function(city) {
-        return city.distance;
-      });
-      
-      city = closest.name;
-    } else {
-      // Sometimes Meetup does not set the lat lon,
-      // so we will try to figure it out from the city of their venue.
-      if(venue.city == "Copenhagen") {
-        city = "København";
-      }
-      else if(venue.city == "Aarhus") {
-        city = "Aarhus";
-      }
-      else if(venue.city == "Esbjerg") {
-        city = "Esbjerg";
-      }
-      else if(venue.city == "Odense") {
-        city = "Odense";
-      }
-      else if(venue.city == "København") {
-        city = "København";
-      }
-      else {
-        city = groupInformation.city; 
-      }
-    }
+    groupInformation.city = findCityFromVenue(venue) || groupInformation.city;
   }
   // END TODO
   
@@ -96,6 +52,54 @@ module.exports.getMeetingsFromGroup = function(groupInformation, meetupKey, call
     callback(null, output);
   });
 };
+
+function findCityFromVenue(venue) {
+  var lat = venue.lat;
+  var lon = venue.lon;
+  var city = null;
+  
+  var cities = [
+    { name: "Aarhus", lat: 56.178104, lon: 10.1817985 },
+    { name: "Odense", lat: 55.3842478, lon: 10.3978195 },
+    { name: "København", lat: 55.6712674, lon: 12.5608388 },
+    { name: "Esbjerg", lat: 55.5225198, lon: 8.4635049 },
+    { name: "Aalborg", lat: 57.0268098, lon: 9.90782 }
+  ];
+  
+  cities.forEach(function(city) {
+    city.distance = getDistance(lat, lon, city.lat, city.lon);
+  });
+  
+  var closeCities = cities.filter(function(city) { return city.distance <= 50; });
+  
+  if(closeCities.length > 0) {
+    var closest = _.min(closeCities, function(city) {
+      return city.distance;
+    });
+    
+    city = closest.name;
+  } else {
+    // Sometimes Meetup does not set the lat lon,
+    // so we will try to figure it out from the city of their venue.
+    if(venue.city == "Copenhagen") {
+      city = "København";
+    }
+    else if(venue.city == "Aarhus") {
+      city = "Aarhus";
+    }
+    else if(venue.city == "Esbjerg") {
+      city = "Esbjerg";
+    }
+    else if(venue.city == "Odense") {
+      city = "Odense";
+    }
+    else if(venue.city == "København") {
+      city = "København";
+    }
+  }
+  
+  return city;
+}
 
 function getDistance(lat1,lon1,lat2,lon2) {
   var R = 6371; // Radius of the earth in km
